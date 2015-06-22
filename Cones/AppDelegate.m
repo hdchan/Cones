2afed5695be28f85cd6bd32ebb538cf29dfad58d
alanscarpa <alan.scarpa@gmail.com>
2015-06-22 15:36:29 -0400

    Retrieving coordinates from Parse

--- Cones/AppDelegate.m
+++ Cones/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,20 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    // [Optional] Power your app with Local Datastore. For more info, go to
+    // https://parse.com/docs/ios_guide#localdatastore/iOS
+    [Parse enableLocalDatastore];
+    
+    // Initialize Parse.
+    [Parse setApplicationId:@"NC80tr9JFvJaAcISZsmGYkdGHRmjghLcLKaZyevm"
+                  clientKey:@"deWWruoWLLEvnzQzsdvfv3VHVmb2pL8WrnBSnwpH"];
+    
+    // [Optional] Track statistics around application opens.
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    
+    
+    
     return YES;
 }
 
--- Cones/ViewController.m
+++ Cones/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import <MapKit/MapKit.h>
 #import "MyAnnotation.h"
+#import <Parse/Parse.h>
 
 
 @interface ViewController () <MKMapViewDelegate, CLLocationManagerDelegate>
@@ -16,7 +17,7 @@
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (nonatomic, strong) NSArray *locations;
 @property (strong, nonatomic) CLLocationManager *locationManager;
-
+@property (nonatomic, strong) CLLocation *currentUserLocation;
 @property (nonatomic) BOOL isLocationSet;
 
 @end
@@ -28,8 +29,7 @@
 - (void)viewDidLoad {
     
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    //[self displayEiffelTower];
+    
     self.locationManager = [[CLLocationManager alloc]init];
     self.locationManager.delegate = self;
     self.isLocationSet = NO;
@@ -40,18 +40,9 @@
     [self.locationManager startUpdatingLocation];
     
     
-    // Display current location and zoom once.
-//    CLLocation *location = [self.locationManager location];
-//    CLLocationCoordinate2D coordinate = [location coordinate];
-//    
-//    MyAnnotation *myLocation = [[MyAnnotation alloc] init];
-//    myLocation.longitude = coordinate.longitude;
-//    myLocation.latitude = coordinate.latitude;
-//    
-//    [self.mapView removeAnnotations:self.mapView.annotations];
-//    [self.mapView addAnnotations:@[myLocation]];
-//    [self.mapView showAnnotations:@[myLocation] animated:YES];
+   
     
+
     
 }
 
@@ -66,28 +57,26 @@
     [self.mapView showAnnotations:self.locations animated:YES];
     
 }
-- (void)displayEiffelTower {
-    MyAnnotation *eiffleTower = [[MyAnnotation alloc] init];
-    eiffleTower.longitude = 2.294481;
-    eiffleTower.latitude = 48.85837;
-    
-    
-    
-    self.locations = @[eiffleTower];
-    
-    [self updateMapViewAnnotations];
-}
 
 
 // Wait for location callbacks
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
 {
 
-    NSLog(@"%@", [locations lastObject]);
+    [self updateAndDisplayCurrentUserLocation];
+    [self sendVendorLocation];
+    [self updateAndDisplayVendorLocation];
+  
+}
+
+
+
+
+-(void)updateAndDisplayCurrentUserLocation {
     
+    self.currentUserLocation = [self.locationManager location];
+    CLLocationCoordinate2D coordinate = [self.currentUserLocation coordinate];
     
-    CLLocation *location = [self.locationManager location];
-    CLLocationCoordinate2D coordinate = [location coordinate];
     
     MyAnnotation *myLocation = [[MyAnnotation alloc] init];
     myLocation.longitude = coordinate.longitude;
@@ -101,28 +90,77 @@
         self.isLocationSet = YES;
     }
     
-   
 }
 
 
--(void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    
-    CLLocationCoordinate2D zoomLocation;
+-(void)sendVendorLocation {
     
     CLLocation *location = [self.locationManager location];
     CLLocationCoordinate2D coordinate = [location coordinate];
     
-    zoomLocation.latitude = coordinate.latitude;
-    zoomLocation.longitude= coordinate.longitude;
+    PFObject *vendorLocation = [PFObject objectWithClassName:@"VendorLocationHistory"];
+    vendorLocation[@"geoPoint"] = [PFGeoPoint geoPointWithLatitude:coordinate.latitude longitude:coordinate.longitude];
     
+    [vendorLocation saveInBackground];
     
-    // 2
-    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 0.5*METERS_PER_MILE, 0.5*METERS_PER_MILE);
-    
-    // 3
-    //[_mapView setRegion:viewRegion animated:YES];
     
     
 }
+
+
+-(void)updateAndDisplayVendorLocation{
+    
+    
+    PFGeoPoint *point = [PFGeoPoint geoPointWithLatitude:self.currentUserLocation.coordinate.latitude longitude:self.currentUserLocation.coordinate.longitude];
+    
+    
+    PFQuery *query = [PFQuery queryWithClassName:@"VendorLocationHistory"];
+    
+    // Create a PFQuery asking for all wall posts 100km of the user
+    // We won't be showing all of the posts returned, 100km is our buffer
+    [query whereKey:@"geoPoint" nearGeoPoint:point withinMiles:5];
+    
+    // Include the associated PFUser objects in the returned data
+    // [query includeKey:PAWParsePostUserKey];
+    
+    // Limit the number of wall posts returned to 20
+    query.limit = 20;
+    [query orderByDescending:@"createdAt"];
+    
+    [query getFirstObjectInBackgroundWithBlock:^(PFObject *object, NSError *error) {
+        PFGeoPoint *location = [object objectForKey:@"geoPoint"];
+        NSLog(@"\nLatitude: %f\nLongitude: %f", location.latitude, location.longitude);
+        
+        if (error){
+            NSLog(@"There was an error:  %@", error);
+        }
+        
+    }];
+    
+    //    NSArray *resultsArray = [query findObjects];
+    //    NSLog(@"The most recent location is: %@", [resultsArray lastObject]);
+    //
+}
+
+
+//-(void)viewWillAppear:(BOOL)animated {
+//    [super viewWillAppear:animated];
+//    
+//    CLLocationCoordinate2D zoomLocation;
+//    
+//    CLLocation *location = [self.locationManager location];
+//    CLLocationCoordinate2D coordinate = [location coordinate];
+//    
+//    zoomLocation.latitude = coordinate.latitude;
+//    zoomLocation.longitude= coordinate.longitude;
+//    
+//    
+//    // 2
+//    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 0.5*METERS_PER_MILE, 0.5*METERS_PER_MILE);
+//    
+//    // 3
+//    //[_mapView setRegion:viewRegion animated:YES];
+//    
+//    
+//}
 @end
