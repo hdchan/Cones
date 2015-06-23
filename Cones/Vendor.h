5452e4dc41c37ef63b7d6aa429afd7abc564f5ac
Henry Chan <henry.d.chan@gmail.com>
2015-06-23 17:36:33 -0400

    Adding Vendor Class

---
+++ Cones/Vendor.h
@@ -0,0 +1,14 @@
+//
+//  Vendor.h
+//  Cones
+//
+//  Created by Henry Chan on 6/23/15.
+//  Copyright (c) 2015 Henry Chan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Parse/Parse.h>
+
+@interface Vendor : PFObject
+
+@end
---
+++ Cones/Vendor.m
@@ -0,0 +1,16 @@
+//
+//  Vendor.m
+//  Cones
+//
+//  Created by Henry Chan on 6/23/15.
+//  Copyright (c) 2015 Henry Chan. All rights reserved.
+//
+
+#import "Vendor.h"
+
+
+@implementation Vendor
+
+
+
+@end
